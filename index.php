89625863e9ea7cb978dead0ec85f15a1938349c8
Xavier Emmanuel RATODISOA <112948536+Xavier-r10@users.noreply.github.com>
2022-09-16 16:53:21 +0300

    Modification

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require 'dbcon.php'
 
 ?>
@@ -14,7 +14,12 @@ require 'dbcon.php'
     <title>Informations des étudiants</title>
   </head>
   <body>
-    <div class="container">
+    <div class="container mt-4">
+
+        <?php 
+          include('message.php');
+        ?>
+
       <div class="row">
         <div class="col-md-12">
           <div class="card">
@@ -50,6 +55,13 @@ require 'dbcon.php'
                         <td><?= $student['email'];?></td>
                         <td><?= $student['phone'];?></td>
                         <td><?= $student['course'];?></td>
+                        <td>
+                          <a href="student-view.php?id=<?= $student['id']; ?>" class="btn btn-info btn-sm">Voir</a>
+                          <a href="student-edit.php?id=<?= $student['id']; ?>" class="btn btn-success btn-sm">Editer</a>
+                          <form action="code.php" method="POST" class="d-inline">
+                            <button type="submit" name="delete" value="<?=$student['id'];?>" class="btn btn-danger btn-sm">Supprimer</button>
+                          </form>
+                        </td>
                       </tr>
                       <?php
                     }
